3c17177d1fbebdc29bba0cac4791c945480638bf
Jan Prášil <prasil.honza@gmail.com>
2019-11-14 12:12:00 +0100

    Add decode option

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
-'use strict';
-var strictUriEncode = require('strict-uri-encode');
-var objectAssign = require('object-assign');
-var decodeComponent = require('decode-uri-component');
+"use strict";
+var strictUriEncode = require("strict-uri-encode");
+var objectAssign = require("object-assign");
+var decodeComponent = require("decode-uri-component");
 
 function encoderForArrayFormat(opts) {
 	switch (opts.arrayFormat) {
-		case 'index':
-			return function (key, value, index) {
-				return value === null ? [
-					encode(key, opts),
-					'[',
-					index,
-					']'
-				].join('') : [
-					encode(key, opts),
-					'[',
-					encode(index, opts),
-					']=',
-					encode(value, opts)
-				].join('');
+		case "index":
+			return function(key, value, index) {
+				return value === null
+					? [encode(key, opts), "[", index, "]"].join("")
+					: [
+							encode(key, opts),
+							"[",
+							encode(index, opts),
+							"]=",
+							encode(value, opts)
+					  ].join("");
 			};
 
-		case 'bracket':
-			return function (key, value) {
-				return value === null ? encode(key, opts) : [
-					encode(key, opts),
-					'[]=',
-					encode(value, opts)
-				].join('');
+		case "bracket":
+			return function(key, value) {
+				return value === null
+					? encode(key, opts)
+					: [encode(key, opts), "[]=", encode(value, opts)].join("");
 			};
 
 		default:
-			return function (key, value) {
-				return value === null ? encode(key, opts) : [
-					encode(key, opts),
-					'=',
-					encode(value, opts)
-				].join('');
+			return function(key, value) {
+				return value === null
+					? encode(key, opts)
+					: [encode(key, opts), "=", encode(value, opts)].join("");
 			};
 	}
 }
@@ -45,11 +38,11 @@ function parserForArrayFormat(opts) {
 	var result;
 
 	switch (opts.arrayFormat) {
-		case 'index':
-			return function (key, value, accumulator) {
+		case "index":
+			return function(key, value, accumulator) {
 				result = /\[(\d*)\]$/.exec(key);
 
-				key = key.replace(/\[\d*\]$/, '');
+				key = key.replace(/\[\d*\]$/, "");
 
 				if (!result) {
 					accumulator[key] = value;
@@ -63,10 +56,10 @@ function parserForArrayFormat(opts) {
 				accumulator[key][result[1]] = value;
 			};
 
-		case 'bracket':
-			return function (key, value, accumulator) {
+		case "bracket":
+			return function(key, value, accumulator) {
 				result = /(\[\])$/.exec(key);
-				key = key.replace(/\[\]$/, '');
+				key = key.replace(/\[\]$/, "");
 
 				if (!result) {
 					accumulator[key] = value;
@@ -80,7 +73,7 @@ function parserForArrayFormat(opts) {
 			};
 
 		default:
-			return function (key, value, accumulator) {
+			return function(key, value, accumulator) {
 				if (accumulator[key] === undefined) {
 					accumulator[key] = value;
 					return;
@@ -102,27 +95,29 @@ function encode(value, opts) {
 function keysSorter(input) {
 	if (Array.isArray(input)) {
 		return input.sort();
-	} else if (typeof input === 'object') {
-		return keysSorter(Object.keys(input)).sort(function (a, b) {
-			return Number(a) - Number(b);
-		}).map(function (key) {
-			return input[key];
-		});
+	} else if (typeof input === "object") {
+		return keysSorter(Object.keys(input))
+			.sort(function(a, b) {
+				return Number(a) - Number(b);
+			})
+			.map(function(key) {
+				return input[key];
+			});
 	}
 
 	return input;
 }
 
 function extract(str) {
-	var queryStart = str.indexOf('?');
+	var queryStart = str.indexOf("?");
 	if (queryStart === -1) {
-		return '';
+		return "";
 	}
 	return str.slice(queryStart + 1);
 }
 
 function parse(str, opts) {
-	opts = objectAssign({arrayFormat: 'none'}, opts);
+	opts = objectAssign({ arrayFormat: "none" }, opts);
 
 	var formatter = parserForArrayFormat(opts);
 
@@ -130,22 +125,22 @@ function parse(str, opts) {
 	// https://github.com/sindresorhus/query-string/issues/47
 	var ret = Object.create(null);
 
-	if (typeof str !== 'string') {
+	if (typeof str !== "string") {
 		return ret;
 	}
 
-	str = str.trim().replace(/^[?#&]/, '');
+	str = str.trim().replace(/^[?#&]/, "");
 
 	if (!str) {
 		return ret;
 	}
 
-	str.split('&').forEach(function (param) {
-		var parts = param.replace(/\+/g, ' ').split('=');
+	str.split("&").forEach(function(param) {
+		var parts = (opts.decode ? param.replace(/\+/g, " ") : param).split("=");
 		// Firefox (pre 40) decodes `%3D` to `=`
 		// https://github.com/sindresorhus/query-string/pull/37
 		var key = parts.shift();
-		var val = parts.length > 0 ? parts.join('=') : undefined;
+		var val = parts.length > 0 ? parts.join("=") : undefined;
 
 		// missing `=` should be `null`:
 		// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
@@ -154,71 +149,79 @@ function parse(str, opts) {
 		formatter(decodeComponent(key), val, ret);
 	});
 
-	return Object.keys(ret).sort().reduce(function (result, key) {
-		var val = ret[key];
-		if (Boolean(val) && typeof val === 'object' && !Array.isArray(val)) {
-			// Sort object keys, not values
-			result[key] = keysSorter(val);
-		} else {
-			result[key] = val;
-		}
+	return Object.keys(ret)
+		.sort()
+		.reduce(function(result, key) {
+			var val = ret[key];
+			if (Boolean(val) && typeof val === "object" && !Array.isArray(val)) {
+				// Sort object keys, not values
+				result[key] = keysSorter(val);
+			} else {
+				result[key] = val;
+			}
 
-		return result;
-	}, Object.create(null));
+			return result;
+		}, Object.create(null));
 }
 
 exports.extract = extract;
 exports.parse = parse;
 
-exports.stringify = function (obj, opts) {
+exports.stringify = function(obj, opts) {
 	var defaults = {
 		encode: true,
 		strict: true,
-		arrayFormat: 'none'
+		arrayFormat: "none"
 	};
 
 	opts = objectAssign(defaults, opts);
 
 	if (opts.sort === false) {
-		opts.sort = function () {};
+		opts.sort = function() {};
 	}
 
 	var formatter = encoderForArrayFormat(opts);
 
-	return obj ? Object.keys(obj).sort(opts.sort).map(function (key) {
-		var val = obj[key];
+	return obj
+		? Object.keys(obj)
+				.sort(opts.sort)
+				.map(function(key) {
+					var val = obj[key];
 
-		if (val === undefined) {
-			return '';
-		}
+					if (val === undefined) {
+						return "";
+					}
 
-		if (val === null) {
-			return encode(key, opts);
-		}
+					if (val === null) {
+						return encode(key, opts);
+					}
 
-		if (Array.isArray(val)) {
-			var result = [];
+					if (Array.isArray(val)) {
+						var result = [];
 
-			val.slice().forEach(function (val2) {
-				if (val2 === undefined) {
-					return;
-				}
+						val.slice().forEach(function(val2) {
+							if (val2 === undefined) {
+								return;
+							}
 
-				result.push(formatter(key, val2, result.length));
-			});
+							result.push(formatter(key, val2, result.length));
+						});
 
-			return result.join('&');
-		}
+						return result.join("&");
+					}
 
-		return encode(key, opts) + '=' + encode(val, opts);
-	}).filter(function (x) {
-		return x.length > 0;
-	}).join('&') : '';
+					return encode(key, opts) + "=" + encode(val, opts);
+				})
+				.filter(function(x) {
+					return x.length > 0;
+				})
+				.join("&")
+		: "";
 };
 
-exports.parseUrl = function (str, opts) {
+exports.parseUrl = function(str, opts) {
 	return {
-		url: str.split('?')[0] || '',
+		url: str.split("?")[0] || "",
 		query: parse(extract(str), opts)
 	};
 };
